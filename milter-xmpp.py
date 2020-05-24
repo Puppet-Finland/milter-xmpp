547595f2591ab30f63f36f688825a0f2eb207bf0
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-05-24 10:06:41 +0300

    Refactoring
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- milter-xmpp.py
+++ milter-xmpp.py
@@ -18,10 +18,12 @@ class XmppForwardMilter(Milter.Base):
         self.data = {}
         self.xmpp_message = ""
 
+    @Milter.noreply
     def header(self, field, value):
         self.data[field] = value
         return Milter.CONTINUE
 
+    @Milter.noreply
     def eoh(self):
         """Add a subset of headers to the XMPP message"""
         self.xmpp_message += "Date: %s\n" % (self.data['Date'])
@@ -29,6 +31,7 @@ class XmppForwardMilter(Milter.Base):
         self.xmpp_message += "Subject: %s\n" % (self.data['Subject'])
         return Milter.CONTINUE
 
+    @Milter.noreply
     def body(self, chunk):
         """Add the body of the message to the XMPP message"""
         self.xmpp_message += "\n"
@@ -37,10 +40,11 @@ class XmppForwardMilter(Milter.Base):
 
     def eom(self):
         """Send the message to the XMPP server"""
-        xmpp_agent.send_message(self.xmpp_message)
+        print(self.xmpp_message)
+        #self.xmpp_agent.send_message(self.xmpp_message)
         return Milter.CONTINUE
 
-class XmppAgent:
+class XmppAgent():
     """Send an XMPP message"""
 
     def __init__(self):
@@ -50,24 +54,33 @@ class XmppAgent:
         self.password = config.get("xmpp", "password")
         self.room = config.get("xmpp", "room")
         self.server = config.get("xmpp", "server")
+        self.jid = xmpp.protocol.JID(self.jabberid)
+        self.user = self.jid.getNode()
+        self.resource = self.jid.getResource()
+        self.client = None
+
+    def connect(self):
+        """Connect to the XMPP server"""
+        self.client = xmpp.Client(server=self.server)
+        self.client.connect()
+        self.client.auth(user=self.user, password=self.password, resource=self.resource)
+
+    def become_present(self):
+        """Make us present on the server"""
+        self.client.sendInitPresence(requestRoster=0)
+
+    def join_chatroom(self):
+        """"Join a chatroom"""
+        join = xmpp.Presence(to="%s/%s" % (self.room, self.user))
+        join.setTag('x', namespace='http://jabber.org/protocol/muc')
+        self.client.send(join)
+
+    def leave_chatroom(self):
+        # Make ourselves unavailable (=leave chatroom)
+        client.sendPresence(type='unavailable')
 
     def send_message(self, message):
-        jid = xmpp.protocol.JID(self.jabberid)
-
-        # Connect to XMPP server
-        client = xmpp.Client(server=self.server)
-        client.connect()
-        client.auth(user=jid.getNode(), password=self.password, resource=jid.getResource())
-
-        # Make us "present" on the server
-        client.sendInitPresence(requestRoster=0)
-
-        # Join a chatroom
-        join = xmpp.Presence(to="%s/%s" % (self.room, jid.getNode()))
-        join.setTag('x', namespace='http://jabber.org/protocol/muc')
-        client.send(join)
-
-        # Construct a message
+        """Send a XMPP message"""
         msg = xmpp.protocol.Message(body=message)
         msg.setType('groupchat')
         msg.setTag('x', namespace='http://jabber.org/protocol/muc#user')
@@ -77,9 +90,6 @@ class XmppAgent:
         # server
         client.send(msg)
         time.sleep(3)
-
-        # Make ourselves unavailable (=leave chatroom)
-        client.sendPresence(typ='unavailable')
 
 def main():
   sys.stdout.flush()
@@ -93,6 +103,7 @@ def main():
   milter_timeout = 10
   Milter.factory = XmppForwardMilter
   Milter.runmilter("xmppforwardmilter",'inet:8894',milter_timeout)
+
 
 if __name__ == "__main__":
   main()
