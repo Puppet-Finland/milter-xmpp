556578b025149259af4305d8125f4320964888fb
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-05-24 10:32:14 +0300

    Make XMPP agent a thread
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- xmpp-agent.py
+++ xmpp-agent.py
@@ -5,13 +5,16 @@ import io
 import os
 import queue
 import sys
+import threading
 import time
 import xmpp
 
-class XmppAgent():
+class XmppAgentThread(threading.Thread):
     """Listen for messages and forward them to an XMPP server"""
 
     def __init__(self):
+        threading.Thread.__init__(self)
+
         config = configparser.ConfigParser()
         config.read("milter-xmpp.ini")
         self.jabberid = config.get("xmpp", "jabberid")
@@ -23,27 +26,35 @@ class XmppAgent():
         self.resource = self.jid.getResource()
         self.client = None
 
-    def connect(self):
+    def run(self):
+        self.xmpp_connect()
+        self.xmpp_become_present()
+        self.xmpp_join_chatroom()
+        self.xmpp_send_message("Test")
+        self.xmpp_leave_chatroom()
+        return
+
+    def xmpp_connect(self):
         """Connect to the XMPP server"""
         self.client = xmpp.Client(server=self.server)
         self.client.connect()
         self.client.auth(user=self.user, password=self.password, resource=self.resource)
 
-    def become_present(self):
+    def xmpp_become_present(self):
         """Make us present on the server"""
         self.client.sendInitPresence(requestRoster=0)
 
-    def join_chatroom(self):
+    def xmpp_join_chatroom(self):
         """"Join a chatroom"""
         join = xmpp.Presence(to="%s/%s" % (self.room, self.user))
         join.setTag('x', namespace='http://jabber.org/protocol/muc')
         self.client.send(join)
 
-    def leave_chatroom(self):
+    def xmpp_leave_chatroom(self):
         # Make ourselves unavailable (=leave chatroom)
         self.client.sendPresence(type='unavailable')
 
-    def send_message(self, message):
+    def xmpp_send_message(self, message):
         """Send a XMPP message"""
         msg = xmpp.protocol.Message(body=message)
         msg.setType('groupchat')
@@ -60,12 +71,9 @@ def main():
 
   # Connect to the XMPP server once. We don't want to repeat this on every
   # email.
-  xmpp_agent = XmppAgent()
-  xmpp_agent.connect()
-  xmpp_agent.become_present()
-  xmpp_agent.join_chatroom()
-  xmpp_agent.send_message("Test")
-  #xmpp_agent.leave_chatroom()
+  agent = XmppAgentThread()
+  agent.start()
+
 
   time.sleep(30)
 
